540a27a56e0c45683c88c4907a9117b356e85eab
ecalasans <ericcalasans@gmail.com>
2017-03-31 09:52:20 -0300

    Função swap ok

---
+++ src/QuickSort.java
@@ -0,0 +1,29 @@
+/**
+ * Created by ericcalasans on 31/03/17.
+ */
+
+// Classe Quicksort a ser completada
+class Quicksort {
+
+    static void swap(int[] a, int i, int j) {
+        int temp;
+
+        temp = a[i];
+        a[j] = a[i];
+        a[i] = temp;
+
+    }
+
+    static int partition(int[] a, int l, int r) {
+        return 0; // a ser completada
+    }
+
+    static void quickrec(int[] a, int l, int r) {
+        // a ser completada
+    }
+
+    static void quicksort(int[] a) {
+        // a ser completada
+    }
+
+}
